0792e0890cde3ee2cb8a154ba3f7be09503c34e4
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2023-01-27 01:18:34 +0300

    fix taxi page not fixed

--- cars/urls.py
+++ cars/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from cars.views import index
+from cars.views import index, used_cars, taxi_cars, detail
 
 urlpatterns = [
     path('', index, name='index'),  # main page
-    path('used_cars/', index, name='used_cars'),  # used cars page
-    path('taxi_cars/', index, name='taxi_cars'),  # taxi cars page
+    path('used_cars/', used_cars, name='used_cars'),  # used cars page
+    path('taxi_cars/', taxi_cars, name='taxi_cars'),  # taxi cars page
+    path('detail/', detail, name='detail'),  # detail page
 ]
--- cars/views.py
+++ cars/views.py
@@ -6,8 +6,12 @@ def index(request):
 
 
 def used_cars(request):
-    return render(request, 'cars/index.html')
+    return render(request, 'cars/used_cars.html')
 
 
 def taxi_cars(request):
-    return render(request, 'cars/index.html')
+    return render(request, 'cars/taxi.html')
+
+
+def detail(request):
+    return render(request, 'cars/card_detail.html')
