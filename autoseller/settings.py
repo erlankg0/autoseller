622a7953d04c8710361fbd8ed442f82f38d1d4b8
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2023-02-06 21:31:58 +0300

    Feed XML

--- autoseller/settings.py
+++ autoseller/settings.py
@@ -139,7 +139,7 @@ STATICFILES_FINDERS = [
 # for development server only (manage.py runserver)
 # Media files (user uploaded files)
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'home/django/autoseller/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
